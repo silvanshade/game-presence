453c44466984f7452e4c3495f56955cad6079329
silvanshade <silvanshade@users.noreply.github.com>
2022-07-24 10:14:47 -0600

    Proof of concept

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,9 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
+use discord_rich_presence::{activity::Activity, DiscordIpc, DiscordIpcClient};
 use tauri::Manager;
 
-#[tauri::command]
-async fn fetch_status() -> Result<(), String> {
-  async fn aux() -> anyhow::Result<()> {
+async fn fetch_status() -> anyhow::Result<serde_json::Value> {
     let url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002";
 
     // TODO: create a personal Web API Key at https://steamcommunity.com/dev/apikey
@@ -23,10 +22,14 @@ async fn fetch_status() -> Result<(), String> {
     let json: serde_json::Value = response.json().await?;
     println!("json: {:#?}", json);
 
+    Ok(json)
+}
+
+#[tauri::command]
+async fn fetch_status_command() -> Result<(), String> {
+    fetch_status().await.map_err(|err| err.to_string())?;
     Ok(())  
   }
-  aux().await.map_err(|err| err.to_string())
-}
 
 fn main() -> anyhow::Result<()> {
     let system_tray_menu = tauri::SystemTrayMenu::new()
@@ -62,14 +65,24 @@ fn main() -> anyhow::Result<()> {
             },
             _ => {},
         })
-        .invoke_handler(tauri::generate_handler![fetch_status])
+        .invoke_handler(tauri::generate_handler![fetch_status_command])
         .build(tauri::generate_context!())?;
 
     // hide app from Dock on macOS
     #[cfg(target_os = "macos")]
     app.set_activation_policy(tauri::ActivationPolicy::Accessory);
 
-    app.run(|app_handle, e| match e {
+    let mut client = {
+        // get a client id by registering an application at https://discord.com/developers/applications
+        let client_id = "<FIXME>";
+        DiscordIpcClient::new(client_id)
+    }
+    .map_err(|err| anyhow::Error::msg(err.to_string()))?;
+
+    app.run(move |app_handle, e| match e {
+        tauri::RunEvent::Exit => {
+            client.close().unwrap();
+        },
         tauri::RunEvent::ExitRequested { api, .. } => {
             api.prevent_exit();
         },
@@ -91,6 +104,22 @@ fn main() -> anyhow::Result<()> {
                     .set_title(new_title)
                     .unwrap();
             }
+        },
+        tauri::RunEvent::Ready => {
+            let json = tauri::async_runtime::block_on(fetch_status()).unwrap();
+            let mut state = String::from("Not playing anything");
+            if let Ok(gameextrainfo) = json
+                .get("response")
+                .and_then(|json| json.get("players"))
+                .and_then(|json| json.get(0))
+                .and_then(|json| json.get("gameextrainfo"))
+                .ok_or_else(|| serde::de::Error::custom("foo"))
+                .and_then(|json| serde_json::from_value::<String>(json.clone()))
+            {
+                state = format!("Playing {}", gameextrainfo);
+            }
+            client.connect().unwrap();
+            client.set_activity(Activity::new().state(&state)).unwrap();
         },
         _ => {},
     });
--- src/fetchStatus.ts
+++ src/fetchStatus.ts
@@ -6,6 +6,6 @@ export function setupFetchStatus(element: HTMLButtonElement) {
     // prints to javascript console; open dev tools for app with `⌥⌘I` (macos) or `⇧^I` (other)
     console.log("Hello, World!");
     // prints to stdout in the terminal
-    tauri.invoke("fetch_status");
+    tauri.invoke("fetch_status_command");
   })
 }
