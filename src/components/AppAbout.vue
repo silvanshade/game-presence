303073ad0eadf41b6ef1a5254100a49ad2cdae91
silvanshade <silvanshade@users.noreply.github.com>
2022-08-07 14:42:29 -0600

    WIP

--- src/components/AppAbout.vue
+++ src/components/AppAbout.vue
@@ -3,17 +3,21 @@
     <div class="m-auto mt-6 grid gap-x-2 text-xs">
       <div class="col-span-2 mb-1 text-center text-xl">steam presence</div>
       <div class="col-span-2 mb-4 text-center">{{ about.pkgDescription }}</div>
-      <div>release version:</div>
-      <div class="font-mono text-xs">{{ about.appVersion }}::{{ about.profile }}</div>
-      <div>release platform:</div>
+      <div class="text-right">build:</div>
+      <div class="font-mono text-xs">
+        {{ about.appVersion }}::{{ about.profile }}::{{ about.gitCommitHash.slice(0, 7) }}::{{
+          about.gitDirty ? "dirty" : "clean"
+        }}
+      </div>
+      <div class="text-right">platform:</div>
       <div class="font-mono text-xs">{{ about.target }}::{{ about.cfgOs }}</div>
-      <div>release build time:</div>
+      <div class="text-right">timestamp:</div>
       <div class="font-mono text-xs">
         {{ about.builtTimeUtc }}
       </div>
-      <div>license:</div>
+      <div class="text-right">license:</div>
       <div class="font-mono text-xs">{{ about.pkgLicense }}</div>
-      <div>homepage:</div>
+      <div class="text-right">homepage:</div>
       <div class="font-mono text-xs">
         <a
           href="#"
@@ -39,6 +43,8 @@ interface About {
   pkgRepository: string;
   target: string;
   profile: string;
+  gitDirty: string;
+  gitCommitHash: string;
   builtTimeUtc: string;
   cfgOs: string;
   appVersion: string;
