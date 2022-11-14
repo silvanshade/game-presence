b88b638b724b7fc60a0df5ff40374cc083b50e2f
silvanshade <silvanshade@users.noreply.github.com>
2022-11-14 15:39:21 -0700

    WIP

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -5,37 +5,88 @@
       push
       class="q-mr-md"
     >
-      <q-tooltip style="white-space: nowrap">Add or remove profiles</q-tooltip>
+      <q-tooltip style="white-space: nowrap">add / remove profiles</q-tooltip>
     </q-btn>
     <q-btn
       :icon="matSwitchAccount"
       push
       class="q-mr-md"
     >
-      <q-tooltip style="white-space: nowrap"> Select profile </q-tooltip>
+      <q-tooltip style="white-space: nowrap"> select profile </q-tooltip>
     </q-btn>
     <q-toolbar-title>no profile selected</q-toolbar-title>
     <q-space />
-    <q-btn
-      :icon="matVisibility"
+    <q-btn-toggle
+      v-model="toggleShowHideGameModel"
+      :options="toggleShowHideGameOptions"
       push
+      class="q-mr-md"
     >
-      <q-tooltip style="white-space: nowrap"> Toggle discord presence </q-tooltip>
-    </q-btn>
+      <template #show>
+        <q-icon :name="matImage" />
+        <q-tooltip style="white-space: nowrap"> show this game as presence </q-tooltip>
+      </template>
+      <template #hide>
+        <q-icon :name="matHideImage" />
+        <q-tooltip style="white-space: nowrap"> hide this game as presence </q-tooltip>
+      </template>
+    </q-btn-toggle>
+    <q-btn-toggle
+      v-model="toggleShowHideAllModel"
+      :options="toggleShowHideAllOptions"
+      push
+      class="q-mr-md"
+    >
+      <template #show>
+        <q-icon :name="matVisibility" />
+        <q-tooltip style="white-space: nowrap"> enable presence </q-tooltip>
+      </template>
+      <template #hide>
+        <q-icon :name="matVisibilityOff" />
+        <q-tooltip style="white-space: nowrap"> disable presence </q-tooltip>
+      </template>
+    </q-btn-toggle>
   </q-toolbar>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { matSwitchAccount, matVisibility } from "@quasar/extras/material-icons";
+import { defineComponent, ref } from "vue";
+import {
+  matHideImage,
+  matImage,
+  matSwitchAccount,
+  matVisibility,
+  matVisibilityOff,
+} from "@quasar/extras/material-icons";
 import { symOutlinedSettingsAccountBox } from "@quasar/extras/material-symbols-outlined";
 
 export default defineComponent({
   name: "HeaderBar",
   components: {},
   setup(_props, ctx) {
+    const toggleShowHideGameModel = ref<"show" | "hide">("hide");
+    const toggleShowHideGameOptions = ref<{ value: string; slot: string }[]>([
+      { value: "show", slot: "show" },
+      { value: "hide", slot: "hide" },
+    ]);
+    const toggleShowHideAllModel = ref<"show" | "hide">("show");
+    const toggleShowHideAllOptions = ref<{ value: string; slot: string }[]>([
+      { value: "show", slot: "show" },
+      { value: "hide", slot: "hide" },
+    ]);
     ctx.expose([]);
-    return { matSwitchAccount, matVisibility, symOutlinedSettingsAccountBox };
+    return {
+      matHideImage,
+      matImage,
+      matSwitchAccount,
+      matVisibility,
+      matVisibilityOff,
+      symOutlinedSettingsAccountBox,
+      toggleShowHideGameModel,
+      toggleShowHideGameOptions,
+      toggleShowHideAllModel,
+      toggleShowHideAllOptions,
+    };
   },
 });
 </script>
--- src/pages/SettingsPageActivity.vue
+++ src/pages/SettingsPageActivity.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="fit flex-center column">
-    <q-list>
+    <q-list dense>
       <q-item
         v-ripple
         tag="label"
       >
         <q-item-section>
-          <q-item-label>Enable Discord Rich Presence</q-item-label>
-          <q-item-label caption>Toggle to control showing game activity on Discord</q-item-label>
+          <q-item-label>Display status as Discord rich presence</q-item-label>
+          <q-item-label caption>Toggle to control displaying game activity on Discord</q-item-label>
         </q-item-section>
         <q-item-section avatar>
           <q-toggle
@@ -24,13 +24,28 @@
       >
         <q-item-section>
           <q-item-label>Enable Twitch integration for game assets</q-item-label>
-          <q-item-label caption>Support fetching game assets from Twitch instead of game service</q-item-label>
+          <q-item-label caption>Support fetching assets from Twitch instead of game service</q-item-label>
         </q-item-section>
         <q-item-section avatar>
           <q-toggle
             v-model="activityTwitchIntegrationEnabled"
             color="brand-twitch"
             :icon="mdiTwitch"
+            size="xl"
+          />
+        </q-item-section>
+      </q-item>
+      <q-item
+        v-ripple
+        tag="label"
+      >
+        <q-item-section>
+          <q-item-label>Require each game to be whitelisted for displaying status</q-item-label>
+          <q-item-label caption>Games will not be shown unless individually whitelisted</q-item-label>
+        </q-item-section>
+        <q-item-section avatar>
+          <q-toggle
+            v-model="activityRequireGameWhitelisted"
             size="xl"
           />
         </q-item-section>
@@ -52,6 +67,7 @@ export default defineComponent({
     return {
       activityDiscordPresenceEnabled: ref(false),
       activityTwitchIntegrationEnabled: ref(false),
+      activityRequireGameWhitelisted: ref(false),
       mdiDiscord,
       mdiTwitch,
     };
