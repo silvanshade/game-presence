a761fccd979063896dd46b2f766fc619e1620b0b
silvanshade <silvanshade@users.noreply.github.com>
2022-11-16 15:08:18 -0700

    WIP

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,5 +1,29 @@
 <template>
-  <p>about</p>
+  <div class="fit flex-center column">
+    <div class="q-mb-lg text-center">
+      <div class="text-h3">game presence</div>
+      <div class="text-h6">An app for reporting game status as Discord presence</div>
+    </div>
+    <div
+      class="row"
+      style="font-family: monospace"
+    >
+      <div class="q-mr-md text-right">
+        <div>build 🏗️</div>
+        <div>platform 🖥️</div>
+        <div>timestamp ⏱️</div>
+        <div>license 📜</div>
+        <div>homepage 🏠</div>
+      </div>
+      <div>
+        <div>build</div>
+        <div>platform</div>
+        <div>timestamp</div>
+        <div>license</div>
+        <div>homepage</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
