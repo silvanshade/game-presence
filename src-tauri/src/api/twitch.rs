b9593836026031dc84ce8464690244a398e2aea1
silvanshade <silvanshade@users.noreply.github.com>
2022-11-22 18:53:35 -0700

    WIP

--- src-tauri/src/api/twitch.rs
+++ src-tauri/src/api/twitch.rs
@@ -13,6 +13,9 @@ pub enum Error {
     TauriWindowClose {
         source: tauri::Error,
     },
+    TokioMpscSend {
+        source: tokio::sync::mpsc::error::SendError<()>,
+    },
     TokioOneShotReceive {
         source: tokio::sync::oneshot::error::RecvError,
     },
@@ -22,17 +25,39 @@ pub enum Error {
     },
 }
 
+impl warp::reject::Reject for Error {
+}
+
 const GAME_PRESENCE_CLIENT_ID: &str = "0vvuyyk8c79jvwqwc9b4hmbqb3sjdr";
 
 const AUTHORIZATION_REDIRECT_URL: &str = "http://localhost:3000/api/twitch/authorize";
 
-pub const AUTHORIZATION_COMPLETE_EVENT: &str = "authorization-complete";
+const AUTHORIZATION_WINDOW_CLOSE_EVENT: &str = "authorization-complete";
+
+const AUTHORIZATION_HANDLER_FINISHED_EVENT: &str = "authorization-handler-finished";
+
+pub fn authorization_html_path() -> std::path::PathBuf {
+    [
+        env!("CARGO_MANIFEST_DIR"),
+        "..",
+        "src",
+        "assets",
+        "html",
+        "api",
+        "twitch",
+        "authorize.html",
+    ]
+    .iter()
+    .collect::<std::path::PathBuf>()
+}
 
 pub fn authorization_request_url() -> Result<url::Url, Error> {
     use twitch_oauth2::{tokens::ImplicitUserTokenBuilder, ClientId};
     let client_id = ClientId::from_static(GAME_PRESENCE_CLIENT_ID);
     let redirect_url = authorization_redirect_url()?;
-    let (url, _csrf_token) = ImplicitUserTokenBuilder::new(client_id, redirect_url).generate_url();
+    let (url, _csrf_token) = ImplicitUserTokenBuilder::new(client_id, redirect_url)
+        .force_verify(true)
+        .generate_url();
     Ok(url)
 }
 
@@ -51,7 +76,7 @@ pub async fn authorization_flow<R: tauri::Runtime>(app: &tauri::AppHandle<R>) ->
     rx_handler_ready.await.context(TokioOneShotReceiveSnafu)?;
 
     let window = {
-        let label = "Twitch Integration Authorization";
+        let label = "twitch-integration-authorization";
         let url = tauri::WindowUrl::External(authorization_request_url()?);
         tauri::WindowBuilder::new(app, label, url)
             .build()
@@ -59,7 +84,7 @@ pub async fn authorization_flow<R: tauri::Runtime>(app: &tauri::AppHandle<R>) ->
     };
 
     let (tx_close, rx_close) = tokio::sync::oneshot::channel();
-    window.once(AUTHORIZATION_COMPLETE_EVENT, |_event| {
+    window.once(AUTHORIZATION_WINDOW_CLOSE_EVENT, |_event| {
         tx_close.send(()).unwrap();
     });
 
@@ -69,16 +94,12 @@ pub async fn authorization_flow<R: tauri::Runtime>(app: &tauri::AppHandle<R>) ->
         .await
         .context(TauriSpawnSnafu)??;
 
-    // match access_token_result.await.context(TauriSpawnSnafu)? {
-    //     Ok(access_token) => {
-    //         todo!()
-    //     },
-    //     Err(err) => Err(err),
-    // }
-
-    access_token_result.await.context(TauriSpawnSnafu)?;
-
-    Ok(())
+    match access_token_result.await.context(TauriSpawnSnafu)? {
+        Ok(access_token) => {
+            todo!()
+        },
+        Err(err) => Err(err),
+    }
 }
 
 async fn authorization_flow_redirect_handler<R: tauri::Runtime>(
@@ -87,25 +108,29 @@ async fn authorization_flow_redirect_handler<R: tauri::Runtime>(
 ) -> Result<(), Error> {
     use warp::Filter;
 
-    println!("launching server");
+    let (tx_handler_finished, mut rx_handler_finished) = tokio::sync::mpsc::channel::<()>(1);
 
     let routes = warp::path!("api" / "twitch" / "authorize")
-        .and(warp::query::<std::collections::HashMap<String, String>>())
-        .map(|map| {
-            println!("{:#?}", map);
-            warp::http::Response::builder().body("")
+        .and(warp::query())
+        .and(warp::fs::file(authorization_html_path()))
+        .and_then(move |map: std::collections::HashMap<String, String>, file| {
+            let tx_handler_finished = tx_handler_finished.clone();
+            async move {
+                if map.get("authorization_finished").is_some() {
+                    tx_handler_finished.send(()).await.context(TokioMpscSendSnafu)?;
+                }
+                Ok::<_, warp::reject::Rejection>(file)
+            }
         });
 
     let server = warp::serve(routes)
-        .bind_with_graceful_shutdown(([127, 0, 0, 1], 3000), async {
+        .bind_with_graceful_shutdown(([127, 0, 0, 1], 3000), async move {
             tx_handler_ready.send(()).unwrap();
-            todo!()
+            rx_handler_finished.recv().await;
         })
         .1;
 
     server.await;
-
-    println!("server shutting down");
 
     Ok(())
 }
