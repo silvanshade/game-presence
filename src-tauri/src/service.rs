44d252edfe08c4d3a212be206585eacf092b54a7
silvanshade <silvanshade@users.noreply.github.com>
2022-11-26 20:53:53 -0700

    WIP

--- src-tauri/src/service.rs
+++ src-tauri/src/service.rs
@@ -59,6 +59,7 @@ impl PlatformWebviewExt for PlatformWebview {
         use objc_foundation::{INSString, NSString};
 
         let web_view = self.inner();
+
         unsafe {
             let handler = {
                 let block = ConcreteBlock::new(move || {
@@ -89,34 +90,20 @@ impl PlatformWebviewExt for PlatformWebview {
 #[cfg(target_os = "linux")]
 impl PlatformWebviewExt for PlatformWebview {
     fn navigate(&self, url: url::Url, clear_data_first: bool) -> Result<(), Error> {
-        fn clear_data(this: &PlatformWebview) -> Result<(), Error> {
-            use webkit2gtk::{CookieManagerExt, WebContextExt, WebViewExt};
+        use webkit2gtk::{CookieManagerExt, WebContextExt, WebViewExt};
 
-            let web_view = this.inner();
+        let web_view = self.inner();
 
+        if clear_data_first {
             let context = web_view.context().context(WebKit2GtkWebviewWebContextSnafu)?;
             let manager = context
                 .cookie_manager()
                 .context(WebKit2GtkWebContextCookieManagerSnafu)?;
-
             #[allow(deprecated)]
             manager.delete_all_cookies();
             context.clear_cache();
-
-            Ok(())
         }
-
-        fn load_url(this: &PlatformWebview, url: url::Url) -> Result<(), Error> {
-            use webkit2gtk::WebViewExt;
-            let web_view = this.inner();
-            web_view.load_uri(url.as_str());
-            Ok(())
-        }
-
-        if clear_data_first {
-            clear_data(self)?;
-        }
-        load_url(self, url)?;
+        web_view.load_uri(url.as_str());
 
         Ok(())
     }
@@ -132,10 +119,10 @@ impl PlatformWebviewExt for PlatformWebview {
         };
 
         let url = PCWSTR::from(&HSTRING::from(url.as_str()));
-        let controller = self.controller();
 
         unsafe {
-            let web_view = controller.CoreWebView2().context(WindowsCoreWebView2Snafu)?;
+            let web_view = self.controller().CoreWebView2().context(WindowsCoreWebView2Snafu)?;
+
             if clear_data_first {
                 web_view
                     .CallDevToolsProtocolMethod(
