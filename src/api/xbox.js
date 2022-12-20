f82ef63d187922ae515cf202f7dec3c8dbb73639
silvanshade <silvanshade@users.noreply.github.com>
2022-12-20 12:15:48 -0700

    WIP

--- src/api/xbox.js
+++ src/api/xbox.js
@@ -1,47 +1,69 @@
-import * as msal from "@azure/msal-node";
-import * as api from "@tauri-apps/api";
+// @ts-check
 
-export const configuration: msal.Configuration = {
+const msal = require("@azure/msal-node");
+// const api = require("@tauri-apps/api");
+
+/** @type {msal.Configuration} */
+const configuration = {
   auth: {
     clientId: "6d97ccd0-5a71-48c5-9bc3-a203a183da22",
   },
 };
 
-export class AuthProvider {
-  readonly configuration: msal.Configuration;
-  readonly clientApplication: msal.PublicClientApplication;
-  readonly tokenCache: msal.TokenCache;
-  accountInfo: msal.AccountInfo | null = null;
+class AuthProvider {
+  /** @type {msal.Configuration} */
+  configuration;
+  /** @type {msal.PublicClientApplication} */
+  clientApplication;
+  /** @type {msal.TokenCache} */
+  tokenCache;
+  /** @type {msal.AccountInfo | null} */
+  accountInfo = null;
 
-  constructor(configuration: msal.Configuration) {
+  /**
+   * @param {msal.Configuration} configuration
+   */
+  constructor(configuration) {
     this.configuration = configuration;
     this.clientApplication = new msal.PublicClientApplication(configuration);
     this.tokenCache = this.clientApplication.getTokenCache();
   }
 
-  async login(): Promise<msal.AccountInfo | null> {
+  /**
+   * @returns {Promise<msal.AccountInfo | null>}
+   */
+  async login() {
     const result = await this.getToken({
       scopes: ["xboxlive.signin", "xboxlive.offline_access"],
     });
     return this.handleAuthenticationResult(result);
   }
 
-  async logout(): Promise<void> {
+  /**
+   * @returns {Promise<void>}
+   */
+  async logout() {
     if (this.accountInfo === null) return;
     try {
       if (this.accountInfo.idTokenClaims?.login_hint && this.configuration.auth.authority) {
         const authority = this.configuration.auth.authority;
         const logoutHint = encodeURIComponent(this.accountInfo.idTokenClaims.login_hint);
         const url = `${authority}/oauth2/v2.0/logout?logout_hint=${logoutHint}`;
-        await api.shell.open(url);
+        // await api.shell.open(url);
+        console.log(url);
       }
     } catch (error) {
       console.error(error);
     }
   }
 
-  async getToken(request: { scopes: string[] }): Promise<msal.AuthenticationResult | null> {
-    let authResponse: msal.AuthenticationResult | null = null;
+  /**
+   * @param {{ scopes: string[] }} request
+   * @returns {Promise<msal.AuthenticationResult | null>}
+   */
+  async getToken(request) {
+    /** @type {msal.AuthenticationResult | null} */
+    let authResponse = null;
     const account = this.accountInfo || (await this.getAccount());
     if (account !== null) {
       authResponse = await this.getTokenSilent({ account, ...request });
@@ -51,7 +73,11 @@ export class AuthProvider {
     return authResponse;
   }
 
-  async getTokenSilent(request: msal.SilentFlowRequest): Promise<msal.AuthenticationResult | null> {
+  /**
+   * @param {msal.SilentFlowRequest} request
+   * @returns {Promise<msal.AuthenticationResult | null>}
+   */
+  async getTokenSilent(request) {
     try {
       return await this.clientApplication.acquireTokenSilent(request);
     } catch (error) {
@@ -65,10 +91,16 @@ export class AuthProvider {
     }
   }
 
-  async getTokenInteractive(request: { scopes: string[] }): Promise<msal.AuthenticationResult | null> {
+  /**
+   * @param {{ scopes: string[] }} request
+   * @returns {Promise<msal.AuthenticationResult | null>}
+   */
+  async getTokenInteractive(request) {
     try {
-      const openBrowser = async (url: string): Promise<void> => {
-        await api.shell.open(url);
+      /** @type {(url: string) => Promise<void>} */
+      const openBrowser = async (url) => {
+        // await api.shell.open(url);
+        console.log(url);
       };
       return await this.clientApplication.acquireTokenInteractive({
         ...request,
@@ -79,7 +111,11 @@ export class AuthProvider {
     }
   }
 
-  async handleAuthenticationResult(result: msal.AuthenticationResult | null): Promise<msal.AccountInfo | null> {
+  /**
+   * @param {msal.AuthenticationResult | null} result
+   * @return {Promise<msal.AccountInfo | null>}
+   */
+  async handleAuthenticationResult(result) {
     if (result !== null) {
       this.accountInfo = result.account;
     } else {
@@ -88,16 +124,24 @@ export class AuthProvider {
     return this.accountInfo;
   }
 
-  async getAccount(): Promise<msal.AccountInfo | null> {
+  /**
+   * @returns {Promise<msal.AccountInfo | null>}
+   */
+  async getAccount() {
     const currentAccounts = await this.tokenCache.getAllAccounts();
     if (currentAccounts.length > 1) {
       // FIXME: do we need to handle this case with "consumers" tenant?
       console.debug("Multiple accounts detected; selecting first account");
-      return currentAccounts[0] as msal.AccountInfo;
+      return currentAccounts[0];
     } else if (currentAccounts.length === 1) {
-      return currentAccounts[0] as msal.AccountInfo;
+      return currentAccounts[0];
     } else {
       return null;
     }
   }
 }
+
+module.exports = {
+  AuthProvider,
+  configuration,
+};
